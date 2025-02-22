ecde6961b50363c76cb0bf9c7d4f982611b22324
yehia9o9 <24-101478@students.eui.edu.eg>
2025-02-22 17:38:38 +0200

    Update helpers.py

--- GDG/utils/helpers.py
+++ GDG/utils/helpers.py
@@ -6,7 +6,6 @@ def preprocess_input_for_sympy(expression):
         expression = re.sub(r"(\d+)([a-zA-Z])", r"\1*\2", expression)
         expression = re.sub(r"([a-zA-Z])(\d+)", r"\1*\2", expression)
         expression = re.sub(r"([a-zA-Z])([a-zA-Z])", r"\1*\2", expression)
-        # Retain the caret (^) symbol for exponentiation
         if "=" not in expression and "differentiate" not in expression.lower():
             expression = f"{expression} = 0"
         return expression
